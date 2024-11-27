41a51ea5c10a9ce11c2023b40918c5df9cea856e
ZXMushroom63 <robert.pirtea.junior@gmail.com>
2024-11-27 19:28:56 +0800

    Fix a formatting issue

--- src/editor.js
+++ src/editor.js
@@ -8,7 +8,8 @@ async function findLatestCommit(instance) {
     var { localDataDir, join } = window.__TAURI__.path;
     const appDir = await join(await installationDir(), 'makemeablender', instance.name);
     if (execCommand) {
-        document.querySelector("#branchdate").innerText = (await execCommand('git', ['log', '-1', '--pretty=format:"Last commit was %ar: %s"'], appDir)).stdout || "Pull to see the latest commits.";
+        var stdout = (await execCommand('git', ['log', '-1', '--pretty=format:"Last commit was %ar: %s"'], appDir)).stdout;
+        document.querySelector("#branchdate").innerText = stdout ? stdout.substring(1, stdout.length - 1) : "Pull to see the latest commits.";
     } else {
         document.querySelector("#branchdate").innerText = "Pull to see the latest commits.";
     }
